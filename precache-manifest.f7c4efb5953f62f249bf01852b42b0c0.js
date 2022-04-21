d118c8650079e6381fea6b66097fe1728f5c6a90
Abdul Moiz Rashid <70377003+abdulmoiz-bit@users.noreply.github.com>
2022-04-22 00:56:07 +0500

    Updates

---
+++ precache-manifest.f7c4efb5953f62f249bf01852b42b0c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0410620e8ec7e48abc27f63de132e60",
+    "url": "/expensetracker/index.html"
+  },
+  {
+    "revision": "260c81a0fa78aecf8d71",
+    "url": "/expensetracker/static/css/main.11bee58a.chunk.css"
+  },
+  {
+    "revision": "bd6bd55d0f17c3088198",
+    "url": "/expensetracker/static/js/2.b190f742.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/expensetracker/static/js/2.b190f742.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "260c81a0fa78aecf8d71",
+    "url": "/expensetracker/static/js/main.b8141334.chunk.js"
+  },
+  {
+    "revision": "6e19c00745fac17d6024",
+    "url": "/expensetracker/static/js/runtime-main.47554b7b.js"
+  }
+]);
